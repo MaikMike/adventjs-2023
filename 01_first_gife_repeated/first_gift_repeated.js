837dc2527c1b7b94735073353bfff30433c575db
Miguel Delgado <mdelgado.cab@gmail.com>
2023-12-10 14:33:19 -0600

    chore: added challange #1

---
+++ 01_first_gife_repeated/first_gift_repeated.js
@@ -0,0 +1,11 @@
+function findFirstRepeated(gifts) {
+  const seenGifts = new Set();
+  for (let gift of gifts) {
+    if (seenGifts.has(gift)) {
+      return gift
+    }
+
+    seenGifts.add(gift);
+  }
+  return -1
+}
